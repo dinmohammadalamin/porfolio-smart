93be199ef3ba00a5a43b3a227838f78196030a36
dinmohammadalamin <dinmohammadalamin123@gmail.com>
2024-03-29 16:11:36 +0600

    Second commit

--- js/script.js
+++ js/script.js
@@ -49,19 +49,35 @@ const nav = document.querySelector(".nav"),
 for (let i = 0; i < totalNavList; i++) {
   const a = navList[i].querySelector("a");
   a.addEventListener("click", function () {
+    removeBackSection();
     for (let j = 0; j < totalNavList; j++) {
       if (navList[j].querySelector("a").classList.contains("active")) {
-        allSection[j].classList.add("back-section");
+        addBackSection(j);
+        //
       }
       navList[j].querySelector("a").classList.remove("active");
     }
     this.classList.add("active");
     showSection(this);
+    if (window.innerWidth < 1200) {
+      asideSectionTogglerBtn();
+    }
   });
 }
-function showSection(element) {
+
+function removeBackSection() {
   for (let i = 0; i < totalSection; i++) {
     allSection[i].classList.remove("back-section");
+  }
+}
+
+function addBackSection(num) {
+  allSection[num].classList.add("back-section");
+}
+
+function showSection(element) {
+  for (let i = 0; i < totalSection; i++) {
+    allSection[i].classList.remove("active");
   }
 
   const target = element.getAttribute("href").split("#")[1];
@@ -80,3 +96,23 @@ function asideSectionTogglerBtn() {
     allSection[i].classList.toggle("open");
   }
 }
+/*===============Hire me ========*/
+function updateNav(element) {
+  for (let i = 0; i < totalNavList; i++) {
+    navList[i].querySelector("a").classList.remove("active");
+    const target = element.getAttribute("href").split("#")[1];
+    if (
+      target ===
+      navList[i].querySelector("a").getAttribute("href").split("#")[1]
+    ) {
+      navList[i].querySelector("a").classList.add("active");
+    }
+  }
+}
+document.querySelector(".hire-me").addEventListener("click", function () {
+  const sectionIndex = this.getAttribute("data-section-index");
+  showSection(this);
+  updateNav(this);
+  removeBackSection();
+  addBackSection(sectionIndex);
+});
